99978e63a182af547dba088da22d2d71278d9016
Dominic Brown <yeahdom@gmail.com>
2017-10-04 17:19:39 -0700

    add technique service, about and forum components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+// angular standards
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -5,10 +6,18 @@ import { HttpModule } from '@angular/http';
 import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 
+// firebase
 import { masterFirebaseConfig } from './api-keys';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+// components
 import { FrontComponent } from './front/front.component';
+import { ForumComponent } from './forum/forum.component';
+import { AboutComponent } from './about/about.component';
+
+// services
+import { TechniqueService } from './services/technique.service';
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
@@ -20,7 +29,9 @@ export const firebaseConfig = {
 @NgModule({
   declarations: [
     AppComponent,
-    FrontComponent
+    FrontComponent,
+    ForumComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +41,7 @@ export const firebaseConfig = {
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireDatabaseModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ TechniqueService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,21 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FrontComponent } from './front/front.component';
+import { AboutComponent } from './about/about.component';
+import { ForumComponent } from './forum/forum.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: FrontComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'forum',
+    component: ForumComponent
   },
 ];
 
